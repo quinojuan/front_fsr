d726f27d9dffb0f75778ca1a9681ea87c482e8c5
Juan Quino <quinojuan@gmail.com>
2023-09-10 00:40:45 -0300

    continuando con las rutas

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import NewPublisher from "./NewPublisher.tsx";
 import NewInforme from "./NewInforme.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/newpublisher",
+    element: <NewPublisher />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <NewPublisher />
-    <NewInforme />
+    <RouterProvider router={router}/>
+      <App />
+      <NewPublisher />
+      <NewInforme />
   </React.StrictMode>
 );
