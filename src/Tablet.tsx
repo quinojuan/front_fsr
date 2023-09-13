5232f4d3894c41fc49b8a4e823495571bd069c35
Juan Quino <quinojuan@gmail.com>
2023-09-12 23:12:49 -0300

    consultas por query x id

--- src/Tablet.tsx
+++ src/Tablet.tsx
@@ -1,69 +1,108 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { useState } from "react";
-import Box from "@mui/material/Box";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import React, { useEffect, useState } from "react";
+import data from "./data.json";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/table";
+import { Input, Button } from "@chakra-ui/react";
 
 const Tablet = () => {
-  
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-      field: 'nombre',
-      headerName: 'Nombre',
-    },
-    {
-      field: 'apellido',
-      headerName: 'Apellido',
-    },
-    {
-      field: 'fecha_nacimiento',
-      headerName: 'Fecha de nacimiento',
-    },
-    {
-      field: 'fecha_bautismo',
-      headerName: 'Fecha de bautismo',
-    },
-    {
-      field: 'esperanza',
-      headerName: 'Esperanza',
-    },
-    {
-      field: 'anciano',
-      headerName: 'Anciano',
-    },
-    {
-      field: 'siervo_ministerial',
-      headerName: 'Siervo Ministerial',
-    },
-    {
-      field: 'precursor_regular',
-      headerName: 'Precursor Regular',
-    },
-    {
-      field: 'precursor_especial',
-      headerName: 'Precursor Especial',
-    },
-    {
-      field: 'grupo',
-      headerName: 'Grupo',
-    },
-  ];
-  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    axios("http://localhost:3000/publicadores").then((response) => {
-      setData(response.data);
-    });
-  }, []);
+  const handleInput = (e: any) => {
+    e.preventDefault();
+    const { value } = e.target;
+    console.log(value);
+    setQuery(value);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      await axios(`http://localhost:3000/consulta?id=${query}`);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
 
   return (
-    <Box >
-      <DataGrid
-        rows={data}
-        columns={columns}
-      />
-    </Box>
+    <>
+      <Input placeholder="Ingrese un dato" w={"15%"} mr={10} onChange={handleInput}/>
+      <Button colorScheme="blue" onClick={handleSubmit}>
+        Enviar consulta
+      </Button>
+      <br />
+      <br />
+      <Table
+        width={"80%"}
+        m={"auto"}
+        variant="simple"
+        colorScheme="blue"
+        size={"sm"}
+      >
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Nombre</Th>
+            <Th>Apellido</Th>
+            <Th>Fecha de nacimiento</Th>
+            <Th>Fecha de bautismo</Th>
+            <Th>Anciano</Th>
+            <Th>Siervo Ministerial</Th>
+            <Th>Precursor Regular</Th>
+            <Th>Precursor Especial</Th>
+            <Th>Grupo</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((item, idx) => (
+            <Tr key={idx}>
+              <Td>{item.id}</Td>
+              <Td>{item.nombre}</Td>
+              <Td>{item.apellido}</Td>
+              <Td>{item.fecha_nacimiento}</Td>
+              <Td>{item.fecha_bautismo}</Td>
+              <Td>
+                {item.anciano === 1 ? (
+                  <input type="checkbox" defaultChecked></input>
+                ) : (
+                  <input type="checkbox" disabled></input>
+                )}
+              </Td>
+              <Td>
+                {item.siervo_ministerial === 1 ? (
+                  <input type="checkbox" defaultChecked></input>
+                ) : (
+                  <input type="checkbox" disabled></input>
+                )}
+              </Td>
+              <Td>
+                {item.precursor_regular === 1 ? (
+                  <input type="checkbox" defaultChecked></input>
+                ) : (
+                  <input type="checkbox" disabled></input>
+                )}
+              </Td>
+              <Td>
+                {item.precursor_especial === 1 ? (
+                  <input type="checkbox" defaultChecked></input>
+                ) : (
+                  <input type="checkbox" disabled></input>
+                )}
+              </Td>
+              <Td>{item.grupo}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </>
   );
 };
 
